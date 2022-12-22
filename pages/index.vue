34dfddefde89ab7e827bf845366a7fc97dbd3e64
Tomspace900 <tomspace.800@gmail.com>
2022-12-22 23:30:17 +0100

    WIP Banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div id="main">
+    <div class="alert" v-if="show">
+      <div class="alert-banner">
+        <button class="alert-close-button" @click="show = false">
+          Fermer X
+        </button>
+      </div>
+      <div class="alert-content">
+        <div class="alert-text">Mon site est encore en travaux !</div>
+      </div>
+    </div>
     <div class="main-sections" id="accueil">
       <img
         src="./../assets/images/Turquoise.webp"
@@ -45,8 +55,10 @@ import Accueil from "../sections/Accueil.vue";
 import Parcours from "../sections/Parcours.vue";
 import Projets from "../sections/Projets.vue";
 
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { useStore } from "@nuxtjs/composition-api";
+
+const show = ref(true);
 
 onMounted(() => {
   useStore().commit(
@@ -57,6 +69,50 @@ onMounted(() => {
 </script>
 
 <style lang="scss">
+.alert {
+  position: fixed;
+  z-index: 100;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 60%;
+  min-width: 900px;
+  height: 280px;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  background-color: #f8c51e;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+
+  &-banner {
+    height: 80px;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+    background: url(https://images.assetsdelivery.com/compings_v2/yevgenijd/yevgenijd1705/yevgenijd170500275.jpg)
+      no-repeat;
+    background-size: cover;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  &-content {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &-close-button {
+    color: white;
+    font-size: 24px;
+    font-weight: bold;
+    cursor: pointer;
+    margin: 20px;
+    text-shadow: 0 0 5px black;
+  }
+}
+
 #main {
   display: flex;
   flex-direction: column;
